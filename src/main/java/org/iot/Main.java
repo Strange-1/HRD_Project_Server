ffb276090203b05e954aa4787b820dbd769e33cd
Strange-1 <strange_1@daum.net>
2022-04-28 17:10:55 +0900

    jdbc connection

--- src/main/java/org/iot/Main.java
+++ src/main/java/org/iot/Main.java
@@ -1,20 +1,53 @@
 package org.iot;
 
+
+import org.sqlite.SQLiteConfig;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    private Connection connection;
+    private boolean isOpened;
+    String dbFileName = "iot";
+    String database = "iot";
+    String tableName = "testTable";
+
+    public static void main(String[] args) {
         System.out.println("Hello world from server");
-        String server = "jdbc:mariadb://127.0.0.7:3306/iot";
-        String database = "iot";
-        String user_name = "iot";
-        String password = "hrd";
-        Connection con = DriverManager.getConnection(server, user_name, password);
-        if (con != null) {
-            System.out.println("JDBC connection: OK");
-            con.close();
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
+    public boolean open() {
+        try { // 읽기 전용
+            SQLiteConfig config = new SQLiteConfig();
+            config.setReadOnly(true);
+            this.connection = DriverManager.getConnection("jdbc:sqlite:/" + this.dbFileName, config.toProperties());
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        isOpened = true;
+        return true;
+    }
+
+    public boolean close() {
+        if (!this.isOpened) {
+            return true;
+        }
+        try {
+            this.connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
 }
