6e30adc1d90f21cdbfbd9b5d0582ed5d6a7775ae
Strange-1 <strange_1@daum.net>
2022-04-28 16:14:58 +0900

    jdbc connection

--- src/main/java/org/iot/Main.java
+++ src/main/java/org/iot/Main.java
@@ -1,7 +1,27 @@
 package org.iot;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world from server");
+        try {
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3307",
+                    "iot",
+                    "hrd"
+            );
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(
+                    "show databases"
+            );
+
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
