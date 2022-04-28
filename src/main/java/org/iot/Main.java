c49a8e76a4f3411992c0ad3a49fe17d2cfcd91c5
Strange-1 <strange_1@daum.net>
2022-04-28 16:35:59 +0900

    jdbc connection

--- src/main/java/org/iot/Main.java
+++ src/main/java/org/iot/Main.java
@@ -1,40 +1,19 @@
 package org.iot;
 
-import org.mariadb.jdbc.Configuration;
-import org.mariadb.jdbc.Connection;
-
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Hello world from server");
-        Connection con = null;
-
         String server = "127.0.0.1:3306";  // 본인의 wsl 서버 ip를 삽입
         String database = "iot";
         String user_name = "iot";
         String password = "hrd";
-
-        try {
-            Class.forName("org.mariadb.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.err.println(" Driver loading error : " + e.getMessage());
-            e.printStackTrace();
-        }
-
-        try {
-            con = org.mariadb.jdbc.Driver.connect(new Configuration.Builder().addHost("127.0.0.1", 3306).user("iot").password("hrd").database("iot").build());
-            System.out.println("Connection Success!");
-        } catch(SQLException e) {
-            System.err.println("Error :" + e.getMessage());
-            e.printStackTrace();
-        }
-        try {
-            if(con != null)
-                con.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+        var con = DriverManager.getConnection(server, user_name, password);
+        if (con != null) {
+            System.out.println("JDBC connection: OK");
+            con.close();
         }
     }
 }
